8a8ad6a7ea94da8b5fc414d8e2be29c84c887028
nino <dankozero111@gmail.com>
2024-11-15 15:31:33 +0300

    [feat]: we mark whe word in the menu list when user's query contains in menu list item

--- src/shared/ui/sideMenuItem/SideMenuItem.tsx
+++ src/shared/ui/sideMenuItem/SideMenuItem.tsx
@@ -8,15 +8,43 @@ type itemProps = {
   content: string;
   menuItemHandler: (value: string) => void;
   currentMenuItem: string | null;
+  menuQuery: string;
 };
 
 export const SideMenuItem = (props: itemProps) => {
-  const { title, content, menuItemHandler, currentMenuItem } = props;
+  const { title, content, menuItemHandler, currentMenuItem, menuQuery } = props;
 
   const currentClass = classNames(
     css.listItem,
     currentMenuItem === title ? css.active : ''
   );
+
+  const getWord = (word: string, query: string) => {
+    const firstIndex = word.toLowerCase().indexOf(query);
+    const lastIndex = firstIndex + query.length;
+
+    const sentenceBeginning = word.slice(0, firstIndex);
+    const sentenceEnding = word.slice(lastIndex);
+    const markedWord = (
+      <span className={css.marked}>{word.slice(firstIndex, lastIndex)}</span>
+    );
+
+    return (
+      <>
+        {sentenceBeginning}
+        {markedWord}
+        {sentenceEnding}
+      </>
+    );
+  };
+
+  const getMarkedWord = (word: string) => {
+    const currentWord = word.trim();
+    const query = menuQuery.toLowerCase().trim();
+
+    if (!currentWord.toLowerCase().includes(query)) return word;
+    return getWord(currentWord, query);
+  };
 
   return (
     <>
@@ -24,7 +52,7 @@ export const SideMenuItem = (props: itemProps) => {
         <span className={css.arrow}>
           <IconImg type="arrow" />
         </span>
-        {title}
+        {getMarkedWord(title)}
       </li>
       {currentMenuItem === title && <p className={css.content}>{content}</p>}
     </>
