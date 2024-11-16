fc20ab9ab8f21aa06e1f1fba5c8726350e0476f9
nino <dankozero111@gmail.com>
2024-11-16 14:04:15 +0300

    [fix]: fix positioning and get sidebar width for main content width calculation

--- src/widgets/sideBar/ui/Sidebar.tsx
+++ src/widgets/sideBar/ui/Sidebar.tsx
@@ -1,18 +1,20 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 
 import { BookmarksSideBar } from '@entities/bookmarksContentSideBar';
 import { MenuSideBar } from '@entities/menuContentSideBar';
 import { IconImg } from '@shared/ui';
+import { isMobileScreen } from '@shared/lib/screenWidth';
 import { sideBarMenuList, menuType } from '../lib/menu';
 
 import css from './sidebar.module.css';
 
-export const SideBar = () => {
-  const isMobile = window.innerWidth <= 768;
-  const [menuItem, setMenuItem] = useState<string | null>('menu');
+type sideBarProps = { onGetWidth: (value: number) => void };
 
-  if (isMobile) return null;
+export const SideBar = ({ onGetWidth }: sideBarProps) => {
+  const isMobile = isMobileScreen();
+  const sideBarRef = useRef<HTMLElement>(null);
+  const [menuItem, setMenuItem] = useState<string | null>('menu');
 
   const onSetMenu = (value: menuType) =>
     setMenuItem(value === menuItem ? null : value);
@@ -20,15 +22,20 @@ export const SideBar = () => {
   const setClName = (value: menuType) =>
     classNames(css.navLink, value === menuItem ? css.active : '');
 
+  useEffect(() => {
+    if (sideBarRef.current) onGetWidth(sideBarRef.current.offsetWidth);
+  }, [menuItem]);
+
+  if (isMobile) return null;
   return (
-    <aside className={css.wrapper}>
+    <aside className={css.wrapper} ref={sideBarRef}>
       <nav className={css.nav}>
         <ul className={css.navList}>
-          {sideBarMenuList.map(({ title, type }) => (
+          {sideBarMenuList.map(({ title, type }, i) => (
             <li
               className={css.navElement}
               onClick={() => onSetMenu(type)}
-              key={title}
+              key={title + i.toString()}
             >
               <a className={setClName(type)}>
                 <IconImg type={type} />
