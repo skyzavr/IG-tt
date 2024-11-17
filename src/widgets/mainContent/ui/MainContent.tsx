e6de749deb6df0a08878d7eda4c1f68ab35beaed
nino <dankozero111@gmail.com>
2024-11-17 13:25:04 +0300

    [fix]: stop sending the form

--- src/widgets/mainContent/ui/MainContent.tsx
+++ src/widgets/mainContent/ui/MainContent.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 import { BtnWrapper } from '@features/mainContent-BtnWrapper';
 import { FormContent } from '@entities/formContent';
@@ -15,13 +15,17 @@ export const MainContent = ({ sideBarWidth }: mainContentProps) => {
     setTaskList({ ...taskList, [type]: value });
   };
 
+  const onSendForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <section
       className={css.wrapper}
       style={{ width: `calc(100% - ${sideBarWidth}px)` }}
     >
       <BtnWrapper isCreate />
-      <form className={css.content}>
+      <form className={css.content} onSubmit={onSendForm}>
         <h1 className={css.title}>{taskList.title}</h1>
         <FormContent taskList={taskList} onUpdateData={onUpdateData} />
       </form>
